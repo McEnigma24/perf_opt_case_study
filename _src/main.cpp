cf1fe02e19b05066e0324edff4036a46bf0279cc
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-06-03 04:44:44 +0200

    quick

--- _src/main.cpp
+++ _src/main.cpp
@@ -1,49 +1,76 @@
 #include "__preprocessor__.h"
 
-#ifdef BUILD_EXECUTABLE
-int main(int argc, char* argv[])
+void array_of_structures()
 {
-    srand(time(NULL));
-
-    struct Positions
+    struct Point
     {
         float x;
         float y;
+
+        char padding[64];
     };
 
-    struct Objects
-    {
-        Positions pos;
-        float radius;
+    size_t size = 100'000'000;
 
-        char padding[16];
-    };
-
-    // size_t size = 800'000'000;
-    size_t size = 1'000'000'000;
-
-    Objects* objects = new Objects[size];
+    Point* objects = new Point[size];
     time_stamp("allocation done");
-    memset(objects, 0, sizeof(Objects) * size);
+    memset(objects, 0, sizeof(Point) * size);
     time_stamp("memset completed");
 
     float rn[3] = {rand() / (float)RAND_MAX, rand() / (float)RAND_MAX, rand() / (float)RAND_MAX};
     for (size_t i = 0; i < size; i++)
     {
-        objects[i].pos.x += rn[0];
-        objects[i].pos.y += rn[1];
-        objects[i].radius += rn[2];
+        objects[i].x += rn[0];
+        objects[i].y += rn[1];
     }
 
     time_stamp("addition done");
 
-    04 : 15 : 01(+214micro 708nano)
+    varr(objects[0].x);
+    var(objects[0].y);
+}
 
-        TIME_STAMP : memset completed 04 : 15 : 18(+16s 300mili 590micro 241nano)
+void structure_of_arrays()
+{
+    struct Point
+    {
+        float* x;
+        float* y;
 
-                         TIME_STAMP : addition done 04 : 15 : 54(+36s 579mili 188micro 36nano)
+        char padding[64];
+    };
 
-                                          var(objects[0].pos.x);
+    size_t size = 100'000'000;
+
+    Point points;
+    points.x = new float[size];
+    points.y = new float[size];
+    time_stamp("allocation done");
+
+    memset(points.x, 0, sizeof(float) * size);
+    memset(points.y, 0, sizeof(float) * size);
+    time_stamp("memset completed");
+
+    float rn[3] = {rand() / (float)RAND_MAX, rand() / (float)RAND_MAX, rand() / (float)RAND_MAX};
+    for (size_t i = 0; i < size; i++)
+    {
+        points.x[i] += rn[0];
+        points.y[i] += rn[1];
+    }
+
+    time_stamp("addition done");
+
+    var(points.x[0]);
+}
+
+#ifdef BUILD_EXECUTABLE
+int main(int argc, char* argv[])
+{
+    srand(time(NULL));
+
+    array_of_structures();
+
+    structure_of_arrays();
 
     return 0;
 }
--- profile.sh
+++ profile.sh
@@ -7,6 +7,9 @@ source config
 
 # perf stat $DIR_TARGET/PERF_CASE_STUDY.exe # nie dla WSL
 
+# perf record -D 100 ./$DIR_TARGET/PERF_CASE_STUDY.exe 1000000 10000
+# perf annotate -Mintel
+
 clear_file "gprof_output.png"; clear_file "gmon.out"; clear_file "gprof_output.txt"; 
 # gprof $DIR_TARGET/PERF_CASE_STUDY.exe gmon.out > gprof_output.txt
 gprof $DIR_TARGET/PERF_CASE_STUDY.exe gmon.out | gprof2dot -w -s | dot -Tpng -o gprof_output.png
