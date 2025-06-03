a07f817dce88bebfdfddca84bfece1abf0b272a1
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-06-03 03:51:22 +0200

    quick

--- _src/main.cpp
+++ _src/main.cpp
@@ -1,28 +1,50 @@
 #include "__preprocessor__.h"
 
-class test
+void func1()
 {
     int tab[100];
-};
+    for (int i = 0; i < 100; i++)
+    {
+        tab[i] = rand() % 100;
+    }
+    var(tab[0]);
+}
+
+void func2()
+{
+    int tab[100];
+    for (int i = 0; i < 100; i++)
+    {
+        tab[i] = rand() % 100;
+    }
+    var(tab[1]);
+}
+
+void func3()
+{
+    int tab[100];
+    for (int i = 0; i < 100; i++)
+    {
+        tab[i] = rand() % 100;
+    }
+    var(tab[2]);
+}
 
 #ifdef BUILD_EXECUTABLE
 int main(int argc, char* argv[])
 {
     srand(time(NULL));
     // CORE::clear_terminal(); // tests will NOT be VISIBLE with this line
-    time_stamp("It just works");
 
     CORE::str::split_string("Hello World!", ' ');
-    var(CORE::str::to_lower_case("Hello, World!"));
+    cout << CORE::str::to_lower_case("Hello, World!") << endl;
 
-    show_sizeof(test);
-    show_sizeof_many(test, 100);
-
-    int num = 123456789;
-    double num2 = 1234567.89123;
-
-    cout << CORE::format_number(num) << endl;
-    cout << CORE::format_number(num2) << endl;
+    for (int i = 0; i < 100; i++)
+    {
+        func1();
+        func2();
+        func3();
+    }
 
     return 0;
 }
--- profile.sh
+++ profile.sh
@@ -7,7 +7,7 @@ source config
 
 # perf stat $DIR_TARGET/PERF_CASE_STUDY.exe # nie dla WSL
 
-# clear_file "gprof_output.png"; clear_file "gmon.out"; clear_file "gprof_output.txt"; 
+clear_file "gprof_output.png"; clear_file "gmon.out"; clear_file "gprof_output.txt"; 
 # gprof $DIR_TARGET/PERF_CASE_STUDY.exe gmon.out > gprof_output.txt
 gprof $DIR_TARGET/PERF_CASE_STUDY.exe gmon.out | gprof2dot -w -s | dot -Tpng -o gprof_output.png
 
