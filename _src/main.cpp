fc587d731b25d75cc78b145a260639be1065fd74
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-06-03 03:57:45 +0200

    quick

--- _src/main.cpp
+++ _src/main.cpp
@@ -1,50 +1,23 @@
 #include "__preprocessor__.h"
 
-void func1()
-{
-    int tab[100];
-    for (int i = 0; i < 100; i++)
-    {
-        tab[i] = rand() % 100;
-    }
-    var(tab[0]);
-}
-
-void func2()
-{
-    int tab[100];
-    for (int i = 0; i < 100; i++)
-    {
-        tab[i] = rand() % 100;
-    }
-    var(tab[1]);
-}
-
-void func3()
-{
-    int tab[100];
-    for (int i = 0; i < 100; i++)
-    {
-        tab[i] = rand() % 100;
-    }
-    var(tab[2]);
-}
-
 #ifdef BUILD_EXECUTABLE
 int main(int argc, char* argv[])
 {
     srand(time(NULL));
-    // CORE::clear_terminal(); // tests will NOT be VISIBLE with this line
 
-    CORE::str::split_string("Hello World!", ' ');
-    cout << CORE::str::to_lower_case("Hello, World!") << endl;
-
-    for (int i = 0; i < 100; i++)
+    struct Positions
     {
-        func1();
-        func2();
-        func3();
+        float x;
+        float y;
     }
+
+    struct Objects
+    {
+        Positions pos;
+        float radius;
+    };
+
+    for (int i = 0; i < 1'000; i++) {}
 
     return 0;
 }
